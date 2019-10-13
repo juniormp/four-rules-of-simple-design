7160b23ebf9116224c6235e45a3bbf27871b8dda
Maurício Junior <junior9771@gmail.com>
2019-10-13 11:44:10 -0300

    Add neighbor function

--- src/main/kotlin/World.kt
+++ src/main/kotlin/World.kt
@@ -18,4 +18,8 @@ class World {
 class LivingCell(var location: Location)
 class DeadCell(var location: Location)
 
-data class Location(val x: Int, val y: Int)
+data class Location(val x: Int, val y: Int){
+    fun neighbors(){
+        // Calculate a list of locations that are considered neighbors
+    }
+}
